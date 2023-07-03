ba76103250772869c570a760fd48160f2d826eb5
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-07-03 16:19:00 -0600

    lab completed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    badge = f'Hello, my name is {name}.'
+    return badge
 
 def batch_badge_creator(names):
-    return None
+    l = []
+    for name in names:
+        l.append(f'Hello, my name is {name}.')
+    return l
 
 def assign_rooms(names):
-    return None
+    l = []
+    index = 0
+    while index < 8:
+        l.append(f"Hello, {names[index]}! You'll be assigned to room {index + 1}!")
+        index += 1
+    return l
 
 def printer(names):
+    for output in batch_badge_creator(names):
+        print(output)
+    for output in assign_rooms(names):
+        print(output)
     return None
